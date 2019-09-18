95cf05059ee4b03489765f3ae8baeef9013a4814
kensuke-tanaka <54127912+kensuke-tanaka@users.noreply.github.com>
2019-09-18 20:31:27 +0900

    2-b

---
+++ Range.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[6]:
+
+
+def range(a,b,c):
+    if 0<=a<=100 and 0<=b<=100 and 0<=c<=100:
+        if a<b<c:
+            print("Yes")
+        else:
+            print("No")
+
+
+# In[7]:
+
+
+range(1,3,8)
+
+
+# In[8]:
+
+
+range(3,8,1)
+
