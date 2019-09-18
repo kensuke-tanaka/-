e6db1cb7a00e55ab2a1b3e3eeb328633f1be42af
kensuke-tanaka <54127912+kensuke-tanaka@users.noreply.github.com>
2019-09-18 21:10:32 +0900

    4-b

---
+++ Circle.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+import math
+def circle(r):
+    if 0<=r<=10000:
+        enshu = 2*r*math.pi
+        menseki = r*r*math.pi
+        print(enshu," ",menseki)
+
+
+# In[5]:
+
+
+circle(2)
+
