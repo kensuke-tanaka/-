4d8ecb9b89f1bf2f60a120de379c5b2a3282f1dd
kensuke-tanaka <54127912+kensuke-tanaka@users.noreply.github.com>
2019-09-18 20:53:14 +0900

    3-c

---
+++ Swapping+Two+Numbers.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+while True:
+    x, y = map(int, input().split())
+    if x == 0 and y == 0:
+        break
+    if x > y :
+        print("{0} {1}".format(y,x))
+    else:
+        print("{0} {1}".format(x,y))
+        
+
