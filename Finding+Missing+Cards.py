9fd2be10786689e38994f24077598d3a97e9c476
kensuke-tanaka <54127912+kensuke-tanaka@users.noreply.github.com>
2019-09-19 04:39:47 +0900

    6-b

---
+++ Finding+Missing+Cards.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+suits = ['S','H','C','D']
+ 
+table = [[False]*14 for i in range(4)]
+ 
+N = int(input())
+ 
+for x in range(N):
+ 
+    mark,num = input().split()
+    num = int(num)
+ 
+    if mark == 'S':
+        table[0][num] = True
+    elif mark == 'H':
+        table[1][num] = True
+    elif mark == 'C':
+        table[2][num] = True
+    else:
+        table[3][num] = True
+        
+for i in range(4):
+    for k in range(1,14):
+        if table[i][k] == False:
+            print("%s %d"%(suits[i],k))
+
